ad8129ceb1b6e7d5198b4f9ff6b220e25cbefed4
Babitovec <babitovec@mail.ru>
2024-08-06 23:38:12 +0400

    pzdc

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ import App from "./app.js"; // Подключение app.js
 const app = ReactDOMClient.createRoot(document.getElementById("app")) //создаем путь где будем выводить контент и указываем его как константу
 
 app.render(<App />) //выводим в div <> то что внутри функции
+
+//pzdc
