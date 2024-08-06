440289582fc773a5ab93233e851832a8d2b6b522
Babitovec <babitovec@mail.ru>
2024-08-07 00:55:57 +0400

    добавил вибрацию при клейме

--- src/components/frens.js
+++ src/components/frens.js
@@ -1,79 +1,87 @@
-import React from "react"
-import  "../css/frens.css" 
+import React from "react";
+import "../css/frens.css";
 
 // Images
 import masks_animated from '../img/masks_animated.gif';
-import flame_emoji from "../img/flame_emoji.webp"
- 
+import flame_emoji from "../img/flame_emoji.webp";
+
+const tg = window.Telegram.WebApp;
+
 class Frens extends React.Component {
+    handleClick = () => {
+        // Вызываем haptic feedback при нажатии
+        tg.hapticFeedback.notificationOccurred('success');
+        // Здесь можно добавить дополнительные действия при нажатии на кнопку
+    }
+
     render() {
-      return (
-        <div>
-            <div className="container">
-                <img src={masks_animated} alt="masks_animated" className="masks-emoji-animated" />
-                <span className="frens-header">Invite Friends<br />and get more Flame</span>
-                <div className="claim-frens">
-                    <div className="claimable-burn">
-                        <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
-                        <div className="claimable-burn-count">156</div>
-                    </div>
-                    <div className="claim-button">
-                        <div className="claim">Claim</div>
-                    </div>
-                </div>
-                <div className="frens-discription">Score 10% for each friend + 🎁</div>
-                <div className="frens-count">3 Friends</div>
-
-                <div className="frens-invited-container">
-                    <div className="invited-fren">
-                        <div className="fren-info">
-                            <div className="username-and-frens-count">
-                                <div className="fren-username">zan_zannn</div>
-                                <div className="username-frens-count">+4</div>
-                            </div>
-                            <div className="fren-total-earned-container">
-                                <div className="fren-total-earned">4679</div>
-                                <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
-                            </div>
+        return (
+            <div>
+                <div className="container">
+                    <img src={masks_animated} alt="masks_animated" className="masks-emoji-animated" />
+                    <span className="frens-header">Invite Friends<br />and get more Flame</span>
+                    <div className="claim-frens">
+                        <div className="claimable-burn">
+                            <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
+                            <div className="claimable-burn-count">156</div>
+                        </div>
+                        <div className="claim-button" onClick={this.handleClick}>
+                            <div className="claim">Claim</div>
                         </div>
                     </div>
-                    
-                    <div className="invited-fren">
-                        <div className="fren-info">
-                            <div className="username-and-frens-count">
-                                <div className="fren-username">Babitovec</div>
-                                <div className="username-frens-count">+3</div>
-                            </div>
-                            <div className="fren-total-earned-container">
-                                <div className="fren-total-earned">3201</div>
-                                <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
+                    <div className="frens-discription">Score 10% for each friend + 🎁</div>
+                    <div className="frens-count">3 Friends</div>
+
+                    <div className="frens-invited-container">
+                        <div className="invited-fren">
+                            <div className="fren-info">
+                                <div className="username-and-frens-count">
+                                    <div className="fren-username">zan_zannn</div>
+                                    <div className="username-frens-count">+4</div>
+                                </div>
+                                <div className="fren-total-earned-container">
+                                    <div className="fren-total-earned">4679</div>
+                                    <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
+                                </div>
                             </div>
                         </div>
+                        
+                        <div className="invited-fren">
+                            <div className="fren-info">
+                                <div className="username-and-frens-count">
+                                    <div className="fren-username">Babitovec</div>
+                                    <div className="username-frens-count">+3</div>
+                                </div>
+                                <div className="fren-total-earned-container">
+                                    <div className="fren-total-earned">3201</div>
+                                    <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="invited-fren">
+                            <div className="fren-info">
+                                <div className="username-and-frens-count">
+                                    <div className="fren-username">KryakZXD</div>
+                                    <div className="username-frens-count">+2</div>
+                                </div>
+                                <div className="fren-total-earned-container">
+                                    <div className="fren-total-earned">2172</div>
+                                    <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
-                    <div className="invited-fren">
-                        <div className="fren-info">
-                            <div className="username-and-frens-count">
-                                <div className="fren-username">KryakZXD</div>
-                                <div className="username-frens-count">+2</div>
-                            </div>
-                            <div className="fren-total-earned-container">
-                                <div className="fren-total-earned">2172</div>
-                                <img src={flame_emoji} alt="burn_logo" className="burn-logo-claim" />
-                            </div>
-                        </div>
+                    <div className="invite-fren-button">
+                        <div className="invite-fren-text">Invite Friends</div>
                     </div>
 
                 </div>
-
-                <div className="invite-fren-button">
-                    <div className="invite-fren-text">Invite Friends</div>
-                </div>
-
             </div>
-        </div>
         );
     }
 }
 
-export default Frens
+export default Frens;
