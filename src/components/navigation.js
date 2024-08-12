23f560e6ed5b583fd85e802ded0b1d5e5a0b2cd2
Babitovec <babitovec@mail.ru>
2024-08-12 22:04:46 +0400

    Вернул вибрацию нав

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -15,29 +15,35 @@ import tasks_icon from '../img/navigation/tasks_icon.webp';
 import friends_icon from '../img/navigation/friends_icon.webp';
 import stats_icon from '../img/navigation/stats_icon.webp';
 
+const tg = window.Telegram.WebApp;
+
 function Navigation() {
   const location = useLocation(); // Получаем текущий маршрут
+
+  const handleNavigationClick = () => {
+    tg.HapticFeedback.impactOccurred('light');
+  };
 
   return (
     <>
       <nav className="navigation">
 
-        <NavLink to="/Home" className="nav-item">
+        <NavLink to="/Home" className="nav-item" onClick={handleNavigationClick}>
           <img src={home_icon} alt="home" className="home_icon" />
           Home
         </NavLink>
 
-        <NavLink to="/Tasks" className="nav-item">
+        <NavLink to="/Tasks" className="nav-item" onClick={handleNavigationClick}>
           <img src={tasks_icon} alt="tasks" className="tasks_icon" />
           Tasks
         </NavLink>
 
-        <NavLink to="/Frens" className="nav-item">
+        <NavLink to="/Frens" className="nav-item" onClick={handleNavigationClick}>
           <img src={friends_icon} alt="friends" className="friends_icon" />
           Frens
         </NavLink>
 
-        <NavLink to="/Stats" className="nav-item">
+        <NavLink to="/Stats" className="nav-item" onClick={handleNavigationClick}>
           <img src={stats_icon} alt="stats_icon" className="stats_icon" />
           Stats
         </NavLink>
