23afa23a26ba2ef5c9a1f89bab7f603a071cdbbf
Babitovec <babitovec@mail.ru>
2024-08-14 01:28:59 +0400

    Добавил play в хэдэр

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,8 @@ import "../css/home.css";
 // Images
 import background_filled_colour from '../img/home/score_background_430x70_without_fade.webp';
 import burn_emoji_animated from '../img/burn_emoji_animated.gif';
-import flame_emoji from "../img/flame_emoji.webp"
 import gift_emoji_animated from '../img/home/gift_emoji_animated.gif';
+import play_icon from "../img/home/play_icon3.webp"
 
 class Home extends React.Component {
   render() {
@@ -15,7 +15,7 @@ class Home extends React.Component {
           <div className="score-stats">
             <img src={background_filled_colour} alt="background_filled_colour" className="score-background" />
             <div className="score-stats-box">
-              <img src={flame_emoji} alt="burn" className="burn-emoji-score" />
+              <img src={play_icon} alt="paly_icon" className="paly_icon" />
               <div className="score-stats-text">Your Score</div>
             </div>
           </div>
@@ -30,7 +30,6 @@ class Home extends React.Component {
           </div>
 
           <div className="gifts">
-            {/* <img src={tg_background} alt="gifts_background" className="gifts-background" /> */}
             <div className="in-gifts">
               <span className="gifts-header">Gifts</span>
               <div className="gift-gif-and-count">
@@ -42,7 +41,6 @@ class Home extends React.Component {
           </div>
 
           <div className="gifts">
-            {/* <img src={tg_background} alt="gifts_background" className="gifts-background" /> */}
             <div className="in-gifts">
               <span className="gifts-header">Burn</span>
               <div className="gift-gif-and-count">
