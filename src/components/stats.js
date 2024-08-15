18986744736f9bd3aba5e26e6339f16a7e452e64
Babitovec <babitovec@mail.ru>
2024-08-16 01:58:36 +0400

    добавил лидерборд + шаманство с френдами

--- src/components/stats.js
+++ src/components/stats.js
@@ -7,7 +7,7 @@ import crown_emoji_animated_compressed from "../img/crown_emoji_animated_compres
 class Stats extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <div className="container-stats">
           <img src={crown_emoji_animated_compressed} alt="cronw_emoji_animated" className="crown-emoji-animated" />
           <span className="stats-header">Leaderboard</span>
@@ -18,9 +18,109 @@ class Stats extends React.Component {
             </div>
             <div className="user-rank">#256791</div>
           </div>
-          <div className="total-users">2M Users</div>
+          <div className="total-users">2M Flamers</div>
+
+          <div className="leaderboard">
+
+            <div className="leaderboard-member">
+              <div>
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+            <div className="leaderboard-member">
+              <div className="leaderboard-member-username-and-flames">
+                <div className="leaderboard-member-username">Abobus</div>
+                <div className="leaderboard-member-flames">20,679,132 FLAMES</div>
+              </div>
+              <div className="rank-leaderbord">#1</div>
+            </div>
+
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
