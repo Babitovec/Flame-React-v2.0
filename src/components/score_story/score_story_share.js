c4dd80b3bc3d5f4f3915eba11c36cc10a9f52e69
Babitovec <babitovec@mail.ru>
2024-09-07 03:26:02 +0400

    добавил и переход на хом и сторис

--- src/components/score_story/score_story_share.js
+++ src/components/score_story/score_story_share.js
@@ -4,14 +4,14 @@ import "../../css/score_story_style/score_story_share.css";
 
 import storyImg from "../../img/story.png"
 
-// const tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;
 
 function ScoreStoryShare() {
     const navigate = useNavigate();
 
     const handleContinue = () => {
         navigate("/Home");
-        // tg.shareToStory('https://funny-hamster-88e22c.netlify.app/static/media/story.466070a59abbf49eb855.png',{text:'Join'});
+        tg.shareToStory('https://funny-hamster-88e22c.netlify.app/static/media/story.466070a59abbf49eb855.png',{text:'Join'});
     };
 
     return (
