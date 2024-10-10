abcba4bc5af48ef97cf3d2cc38f296d2c6216719
Babitovec <babitovec@mail.ru>
2024-10-11 02:35:04 +0400

    еще чек логов

--- src/components/home.js
+++ src/components/home.js
@@ -54,7 +54,7 @@ const Home = () => {
         const userId = tg.initDataUnsafe.user?.id;
         const response = await axios.get(`https://more-gratefully-hornet.ngrok-free.app/users/${userId}`);
         const userData = response.data;
-        console.log('Данные успешно получены:', userData) //Чекаем
+        console.log('Данные успешно получены:', userData.flames_count, userData.gifts_count) //Чекаем
         setFlamesCount(userData.flames_count);
         setGiftsCount(userData.gifts_count);
       } catch (error) {
