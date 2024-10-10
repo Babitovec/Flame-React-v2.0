1ef59752d2162807ffc0884cc05978cea213deff
Babitovec <babitovec@mail.ru>
2024-10-11 01:03:27 +0400

    ngrok test

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const App = () => {
     const userData = tg.initDataUnsafe.user;
 
     if (userData) {
-      axios.post('https://flameapp-babito.amvera.io', userData)
+      axios.post('https://more-gratefully-hornet.ngrok-free.app', userData)
         .then(response => {
           console.log('Данные успешно отправлены:', response.data);
         })
