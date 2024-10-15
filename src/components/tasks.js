2b5b0e62c506686afc337f49b2c11f2c6ce11309
Babitovec <babitovec@mail.ru>
2024-10-15 05:02:25 +0400

    Новый фронт для таскс

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -8,9 +8,54 @@ import x_icon from "../img/tasks/x_icon.webp";
 import flame_emoji from "../img/flame_emoji.webp";
 import wallet_icon from "../img/tasks/wallet_icon.webp";
 import friends_icon from '../img/tasks/friends_icon.png';
-import check_mark from "../img/tasks/check-mark.png"
+import check_mark from "../img/tasks/check-mark.png";
 
 const tg = window.Telegram.WebApp;
+
+const tasksData = [
+  {
+    id: 1,
+    title: "Add 🔥 in Telegram name",
+    points: "+1 Gift 🎁",
+    icon: flame_emoji,
+    onClick: () => {}, // Здесь можно добавить логику выполнения задачи
+  },
+  {
+    id: 2,
+    title: "Invite 5 friends",
+    points: "+5 Gifts 🎁",
+    icon: friends_icon,
+    onClick: () => {}, // Здесь можно добавить логику выполнения задачи
+  },
+  {
+    id: 3,
+    title: "Connect your wallet",
+    points: "+1 Gift 🎁",
+    icon: wallet_icon,
+    onClick: () => {}, // Здесь можно добавить логику выполнения задачи
+  },
+  {
+    id: 4,
+    title: "Subscribe to Flame Telegram",
+    points: "+100 Flame 🔥",
+    icon: tg_icon,
+    onClick: () => tg.openTelegramLink("https://t.me/flame_coin_community"),
+  },
+  {
+    id: 5,
+    title: "Subscribe to Flame X",
+    points: "+100 Flame 🔥",
+    icon: x_icon,
+    onClick: () => tg.openLink("https://x.com/realDogsHouse"),
+  },
+  {
+    id: 6,
+    title: "Share about us on X",
+    points: "+100 Flame 🔥",
+    icon: x_icon,
+    onClick: () => {}, // Здесь можно добавить логику выполнения задачи
+  },
+];
 
 const Tasks = () => {
   tg.setHeaderColor("#000000");
@@ -49,112 +94,34 @@ const Tasks = () => {
     });
   }, []);
 
-  const handleOpenTelegramChannel = () => {
-    tg.openTelegramLink("https://t.me/flame_coin_community");
-  };
-
-  const handleOpenX = () => {
-    tg.openLink("https://x.com/realDogsHouse"); // Заменить на реальный Twitter/X
-  };
-
   if (loading) {
     return (
       <div className="loader-box">
-        <div class="loader"></div>
+        <div className="loader"></div>
       </div>
     );
   }
 
   return (
-    <>
-      <div className="container-tasks">
-        <img src={tasks_gift_emoji_animated} alt="gift" className="tasks_emoji_animated" />
-        <span className="tasks-header">Tasks</span>
-        <span className="tasks-description">Complete tasks and get more Flame.</span>
-        <div className="tasks">
-
-          <div className="task">
-            <img src={flame_emoji} alt="flame_emoji" className="icon" />
+    <div className="container-tasks">
+      <img src={tasks_gift_emoji_animated} alt="gift" className="tasks_emoji_animated" />
+      <span className="tasks-header">Tasks</span>
+      <span className="tasks-description">Complete tasks and get more Flame.</span>
+      <div className="tasks">
+        {tasksData.map((task) => (
+          <div className="task" key={task.id}>
+            <img src={task.icon} alt={`${task.title} icon`} className="icon" />
             <div className="text">
-              <div className="title">Add 🔥 in Telegram name</div>
-              <div className="points">+1 Gift 🎁</div>
+              <div className="title">{task.title}</div>
+              <div className="points">{task.points}</div>
             </div>
-            <div className="open-button">
+            <div className="open-button" onClick={task.onClick}>
               <div className="open">Start</div>
             </div>
           </div>
-
-          <div className="task">
-            <img src={friends_icon} alt="friends_icon" className="icon" />
-            <div className="text">
-              <div className="title">Invite 5 friends</div>
-              <div className="points">+5 Gifts 🎁</div>
-            </div>
-            <div className="open-button">
-              <div className="open">Start</div>
-            </div>
-          </div>
-
-          {/* Connect wallet */}
-          <div className="task">
-            <img src={wallet_icon} alt="wallet_icon" className="icon" />
-            <div className="text">
-              <div className="title">Connect your wallet</div>
-              <div className="points">+1 Gift 🎁</div>
-            </div>
-            <div className="open-button">
-              <div className="open">Start</div>
-            </div>
-          </div>
-
-          {/* Telegram channel subscription */}
-          <div className="task">
-            <img src={tg_icon} alt="tg_icon" className="icon" />
-            <div className="text">
-              <div className="title">Subscribe to Flame Telegram</div>
-              <div className="points">+100 Flame 🔥</div>
-            </div>
-            <div className="open-button" onClick={handleOpenTelegramChannel}>
-              <div className="open">Start</div>
-            </div>
-          </div>
-
-          {/* верстка для состояния заданий
-          <div className="cheking-task">
-            <div class="loader-task"></div>
-          </div>
-
-          <div className="task-done">
-            <img src={check_mark} alt="check_mark" className="check_mark" />
-          </div> */}
-
-          {/* Twitter subscription */}
-          <div className="task">
-            <img src={x_icon} alt="x_icon" className="icon" />
-            <div className="text">
-              <div className="title">Subscribe to Flame X</div>
-              <div className="points">+100 Flame 🔥</div>
-            </div>
-            <div className="open-button" onClick={handleOpenX}>
-              <div className="open">Start</div>
-            </div>
-          </div>
-
-          {/* Twitter share about us */}
-          <div className="task">
-            <img src={x_icon} alt="x_icon" className="icon" />
-            <div className="text">
-              <div className="title">Share about us on X</div>
-              <div className="points">+100 Flame 🔥</div>
-            </div>
-            <div className="open-button">
-              <div className="open">Start</div>
-            </div>
-          </div>
-
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
