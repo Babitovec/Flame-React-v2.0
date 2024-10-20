a9b937a0c3e96a07ba52750cf373bc360da4edfe
Babitovec <babitovec@mail.ru>
2024-10-20 04:18:17 +0400

    test

--- src/components/dailyStreak.js
+++ src/components/dailyStreak.js
@@ -7,44 +7,46 @@ import "../css/dailyStreak.css";
 import gz_daily from '../img/gz-daily.gif';
 import flame_emoji from "../img/flame_emoji.webp";
 import gift_emoji from '../img/home/gift_emoji.webp';
-import confettie from "../img/confetti.json"
+import confettie from "../img/confetti.json";
 
 const tg = window.Telegram.WebApp;
 
-const DailyStreak = ({ setShowDailyStreak }) => {
+const DailyStreak = ({ handleContinue }) => {
     tg.setHeaderColor("#000000");
 
     const navigate = useNavigate();
 
-    const handleContinue = () => {
-        navigate("/Home"); // Навигация на Home
+    const handleContinueClick = () => {
+        handleContinue(); // Вызываем переданный метод для скрытия DailyStreak
+        navigate("/Home"); // Перенаправляем на Home
     };
 
     return (
-        <>
-            <div className="streak-container">
-                <Lottie className="confettie" loop={false} animationData={confettie} />
-                <img src={gz_daily} alt="flame_emoji_animated" className="logo_streak" />
-                <div className="day-streak">2</div>
-                <div className="rewards-title">day check-in</div>
-                <div className="rewards-container">
-                    <div className="reward-box-1">
-                        <img src={flame_emoji} alt="flame_emoji_animated" className="emoji_reward" />
-                        <div className="count-daily-reward">100</div>
-                        <div className="reward-txt">Flames</div>
-                    </div>
-                    <div className="reward-box-2">
-                        <img src={gift_emoji} alt="flame_emoji_animated" className="emoji_reward" />
-                        <div className="count-daily-reward">1</div>
-                        <div className="reward-txt">Gifts</div>
-                    </div>
+        <div className="streak-container">
+            <Lottie className="confettie" loop={false} animationData={confettie} />
+            <img src={gz_daily} alt="flame_emoji_animated" className="logo_streak" />
+            <div className="day-streak">2</div>
+            <div className="rewards-title">day check-in</div>
+            <div className="rewards-container">
+                <div className="reward-box-1">
+                    <img src={flame_emoji} alt="flame_emoji_animated" className="emoji_reward" />
+                    <div className="count-daily-reward">100</div>
+                    <div className="reward-txt">Flames</div>
                 </div>
-                <div className="description-daily-reward">Come back tommorow for check-in day 3 <br />Skipping a day resets your check-in</div>
-                <div className="continue-button-daily-reward" onClick={handleContinue}>
-                    <span className="continue-text" >Continue</span>
+                <div className="reward-box-2">
+                    <img src={gift_emoji} alt="flame_emoji_animated" className="emoji_reward" />
+                    <div className="count-daily-reward">1</div>
+                    <div className="reward-txt">Gifts</div>
                 </div>
             </div>
-        </>
+            <div className="description-daily-reward">
+                Come back tomorrow for check-in day 3 <br />
+                Skipping a day resets your check-in
+            </div>
+            <div className="continue-button-daily-reward" onClick={handleContinueClick}>
+                <span className="continue-text">Continue</span>
+            </div>
+        </div>
     );
 };
 
